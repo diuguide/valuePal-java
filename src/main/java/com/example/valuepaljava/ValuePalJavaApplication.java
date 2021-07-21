a288a61af405bd7a92c37fbecbe3e82ff2dc909c
diuguide <everett.diuguid@gmail.com>
2021-07-21 11:34:47 -0400

    initialized springboot application

---
+++ src/main/java/com/example/valuepaljava/ValuePalJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.valuepaljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuePalJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValuePalJavaApplication.class, args);
+    }
+
+}
