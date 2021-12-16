87b5b86ae9af7e6b315730aa5aa1d56fdda616b1
diuguide <everett.diuguid@gmail.com>
2021-12-15 23:20:06 -0500

    added SQL file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,34 @@
+--
+-- CREATE OR REPLACE PROCEDURE updateavgprice()
+-- LANGUAGE plpgsql
+-- AS $$
+-- DECLARE
+-- 	holding record;
+-- BEGIN
+-- 	for holding in (select * from valuepal.holdings) loop
+-- 		update valuepal.holdings set avg_purchase_price =
+-- 		(select avg(price) from valuepal.orders where ticker = holding.ticker
+-- 		and wallet_id = holding.wallet_id) where ticker=holding.ticker
+-- 		and wallet_id = holding.wallet_id;
+-- 	end loop;
+-- END;
+-- $$
 
-CREATE OR REPLACE PROCEDURE updateavgprice()
+CREATE OR REPLACE FUNCTION valuepaltest.updateavgprice1() RETURNS int
 LANGUAGE plpgsql
 AS $$
 DECLARE
-	holding record;
+holding record;
+	num int;
 BEGIN
-	for holding in (select * from valuepal.holdings) loop
-		update valuepal.holdings set avg_purchase_price =
-		(select avg(price) from valuepal.orders where ticker = holding.ticker 
-		and wallet_id = holding.wallet_id) where ticker=holding.ticker
-		and wallet_id = holding.wallet_id;
-	end loop;
+    for holding in (select * from valuepal.holdings) loop
+        update valuepal.holdings set avg_purchase_price =
+        (select avg(price) from valuepal.orders where ticker = holding.ticker
+        and wallet_id = holding.wallet_id) where ticker=holding.ticker
+        and wallet_id = holding.wallet_id;
+        num = 1;
+            return num;
+    end loop;
 END;
 $$
 
