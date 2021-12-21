f2419aeda26dde46dc43de1a636fe24c5d0e393c
diuguide <everett.diuguid@gmail.com>
2021-12-21 10:06:41 -0500

    apiQueue class created, lazy singleton

--- src/main/java/com/example/valuepaljava/util/ApiQueue.java
+++ src/main/java/com/example/valuepaljava/util/ApiQueue.java
@@ -1,5 +1,6 @@
 package com.example.valuepaljava.util;
 
+import com.example.valuepaljava.exceptions.InvalidInputException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,7 @@ public class ApiQueue {
 
     private final Logger logger = LoggerFactory.getLogger(ApiQueue.class);
     private static ApiQueue apiQueue = null;
-    private static List<ResponseEntity<String>> queue = new LinkedList<>();
+    private static LinkedList<ResponseEntity<String>> queue = new LinkedList<ResponseEntity<String>>();
 
     public static ApiQueue getInstance() {
         if(apiQueue == null) {
@@ -28,9 +29,8 @@ public class ApiQueue {
 
     public ResponseEntity<String> runQueue() {
         while (queue.size() > 0) {
-
+            return queue.getFirst();
         }
+        throw new InvalidInputException("Invalid input!");
     }
-
-
 }
