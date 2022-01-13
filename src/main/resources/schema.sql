6da17c9b507a2a11ebe8fe41aed72195de1b472d
diuguide <everett.diuguid@gmail.com>
2022-01-12 22:50:19 -0500

    formatting, cleaning up SQL schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,93 +1,132 @@
---
--- CREATE OR REPLACE PROCEDURE updateavgprice()
--- LANGUAGE plpgsql
--- AS $$
--- DECLARE
--- 	holding record;
--- BEGIN
--- 	for holding in (select * from valuepal.holdings) loop
--- 		update valuepal.holdings set avg_purchase_price =
--- 		(select avg(price) from valuepal.orders where ticker = holding.ticker
--- 		and wallet_id = holding.wallet_id) where ticker=holding.ticker
--- 		and wallet_id = holding.wallet_id;
--- 	end loop;
--- END;
--- $$
-
 CREATE OR REPLACE FUNCTION valuepaltest.updateavgprice1() RETURNS int
 LANGUAGE plpgsql
 AS $$
 DECLARE
 holding record;
-	num int;
+
+num int;
 BEGIN
-    for holding in (select * from valuepaltest.holdings) loop
-        update valuepaltest.holdings set avg_purchase_price =
-        (select avg(price) from valuepaltest.orders where ticker = holding.ticker
-        and wallet_id = holding.wallet_id) where ticker=holding.ticker
-        and wallet_id = holding.wallet_id;
-        num = 1;
-            return num;
-    end loop;
+	FOR holding IN (
+	SELECT
+		*
+	FROM
+		valuepaltest.holdings) loop
+        UPDATE
+	valuepaltest.holdings
+SET
+	avg_purchase_price =
+        (
+	SELECT
+		avg(price)
+	FROM
+		valuepaltest.orders
+	WHERE
+		ticker = holding.ticker
+		AND wallet_id = holding.wallet_id)
+WHERE
+	ticker = holding.ticker
+	AND wallet_id = holding.wallet_id;
+
+num = 1;
+
+RETURN num;
+END loop;
 END;
+
 $$
 
 CREATE TRIGGER updateavgpricesingleholding
-    AFTER INSERT OR UPDATE
-                        ON valuepaltest.holdings
+    AFTER
+INSERT
+	OR
+UPDATE
+	ON
+	valuepaltest.holdings
                         EXECUTE PROCEDURE valuepaltest.updateavgprice2();
 
 CREATE OR REPLACE FUNCTION valuepaltest.updateavgprice2()
-RETURNS TRIGGER as $updateavgpricesingleholding$
+RETURNS TRIGGER AS $updateavgpricesingleholding$
 DECLARE
-avg_price numeric;
+avg_price NUMERIC;
 BEGIN
-select avg(price) into avg_price from valuepaltest.orders
-where ticker = NEW.ticker
-  and wallet_id = NEW.wallet_id;
-return NEW;
+	SELECT
+	avg(price)
+INTO
+	avg_price
+FROM
+	valuepaltest.orders
+WHERE
+	ticker = NEW.ticker
+	AND wallet_id = NEW.wallet_id;
+
+RETURN NEW;
 END
 $updateavgpricesingleholding$ LANGUAGE plpgsql;
 
-DROP TRIGGER updateavgpricesingleholding on valuepaltest.holdings;
+DROP TRIGGER updateavgpricesingleholding ON
+valuepaltest.holdings;
+
 CREATE TRIGGER updateavgpricesingleholding
-    BEFORE INSERT OR UPDATE
-                         ON valuepaltest.holdings
+    BEFORE
+INSERT
+	OR
+UPDATE
+	ON
+	valuepaltest.holdings
                          FOR EACH ROW
                          EXECUTE PROCEDURE valuepaltest.updateavgprice2();
 
 CREATE OR REPLACE FUNCTION valuepaltest.calcavgprice(
-	p_newprice valuepaltest.holdings.price%type,
-	p_wallet_id valuepaltest.holdings.wallet_id%type,
-	p_ticker valuepaltest.holdings.ticker%type
+	p_newprice valuepaltest.holdings.price%TYPE,
+	p_wallet_id valuepaltest.holdings.wallet_id%TYPE,
+	p_ticker valuepaltest.holdings.ticker%TYPE
 )
 RETURNS DOUBLE PRECISION
 LANGUAGE plpgsql
 AS $$
 DECLARE
-v_avg valuepaltest.holdings.avg_purchase_price%type;
+v_avg valuepaltest.holdings.avg_purchase_price%TYPE;
 BEGIN
-select avg(price) into v_avg from valuepaltest.orders
-where ticker = p_ticker
-  and wallet_id = p_wallet_id;
-return (v_avg + p_newprice) / 2;
+	SELECT
+	avg(price)
+INTO
+	v_avg
+FROM
+	valuepaltest.orders
+WHERE
+	ticker = p_ticker
+	AND wallet_id = p_wallet_id;
+
+RETURN (v_avg + p_newprice) / 2;
 END;
+
 $$
 
 CREATE OR REPLACE FUNCTION valuepaltest.updateavgprice2()
-RETURNS TRIGGER as $updateavgpricesingleholding$
+RETURNS TRIGGER AS $updateavgpricesingleholding$
 DECLARE
 rec_count int;
 BEGIN
-select count(*) into rec_count from valuepaltest.orders
-where ticker = NEW.ticker
-  and wallet_id = NEW.wallet_id;
+	SELECT
+	count(*)
+INTO
+	rec_count
+FROM
+	valuepaltest.orders
+WHERE
+	ticker = NEW.ticker
+	AND wallet_id = NEW.wallet_id;
+
 IF(rec_count > 0) THEN
-		NEW.avg_purchase_price := valuepaltest.calcavgprice(NEW.price, NEW.wallet_id, NEW.ticker);
-return NEW;
+		NEW.avg_purchase_price := valuepaltest.calcavgprice(NEW.price,
+NEW.wallet_id,
+NEW.ticker);
+
+RETURN NEW;
 ELSE
-		NEW.avg_purchase_price:=NEW.price;
-return NEW;
+		NEW.avg_purchase_price := NEW.price;
+
+RETURN NEW;
 END IF;
 END
 $updateavgpricesingleholding$ LANGUAGE plpgsql;
